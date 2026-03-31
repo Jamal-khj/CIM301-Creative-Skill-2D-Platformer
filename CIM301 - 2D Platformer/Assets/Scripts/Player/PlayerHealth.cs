6225e6275f851d58b1c7f10cbd99b79766b415ac
Jamal-khj <114579024+Jamal-khj@users.noreply.github.com>
2026-04-01 01:33:34 +0400

    Player Health Script added

---
+++ CIM301 - 2D Platformer/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private int health;
+    [SerializeField] private int maxHealth;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        if(health < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
